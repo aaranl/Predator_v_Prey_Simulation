3007d526fc3a1fb5b27dc9f925b1664247d084b6
Aaran Lawing <lawingaaran@gmail.com>
2024-02-01 18:34:55 -0500

    update individual files

--- python_files/ecosystemModel.py
+++ python_files/ecosystemModel.py
@@ -1,24 +1,17 @@
-from mesa import Model
-from mesa.time import RandomActivation
-from predator import Predator
-from python_files.prey import Prey
-
-
-
-class EcosystemModel(Model):
-    """A model with predators and prey."""
+class EcosystemModel(mesa.Model):
+    """A model with some number of agents."""
 
     def __init__(self, num_predators, num_prey):
         self.num_predators = num_predators
         self.num_prey = num_prey
-        self.schedule = RandomActivation(self)
-        
-        # Create predators
+        # Create scheduler and assign it to the model
+        self.schedule = mesa.time.RandomActivation(self)
+
+        # Create agents
         for i in range(self.num_predators):
             predator = Predator(i, self)
             self.schedule.add(predator)
         
-        # Create prey
         for i in range(self.num_prey):
             prey = Prey(i + self.num_predators, self)
             self.schedule.add(prey)
--- python_files/predator.py
+++ python_files/predator.py
@@ -1,12 +1,19 @@
-from mesa import Agent
-
-class Predator(Agent):
+class Predator(mesa.Agent):
     """ A predator agent """
     
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         # TODO: Add predator properties here
+        self.is_predator = True
+        self.life = 10
 
     def step(self):
         # TODO: Define predator behavior here
-        pass
+        if self.life > 0: 
+            other_agent = self.random.choice(self.model.schedule.agents)
+            if other_agent is not None and other_agent.is_prey:
+                self.life += 1
+                other_agent.life -= 1
+        
+        
+        
